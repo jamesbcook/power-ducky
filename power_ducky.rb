7d75803eb66095b7ae4af35344f54d316dd16053
b00stfr3ak <goduke44@gmail.com>
2013-09-03 13:27:56 -0700

    Skeleton File Created
    The main file in skeleton format

---
+++ power_ducky.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+require './lib/core'
+require './lib/menu'
+include MainCommands
+include Menu
+def case_main_menu
+  answer = main_menu
+	case answer
+	  when '1'
+      case_reverse_meterpreter_menu
+	  when '2'
+			case_dump_hashes_menu
+		when '3'
+			case_dump_lsass_menu
+		when '99'
+		  exit
+		else
+		  print_error("Bad Choice")
+			sleep(1)
+			case_main_menu
+		end
+end
+def case_reverse_meterpreter_menu
+  reverse_meterpreter_answer = reverse_meterpreter
+	  case reverse_meterpreter_answer
+			when '1'
+		  when '2'
+			when '3'
+			when '99'
+			  case_main_menu
+			else
+        print_error("Bad Choice")
+				sleep(1)
+			  case_reverse_meterpreter_menu
+      end
+end
+def case_dump_hashes_menu
+  reverse_meterpreter_answer = reverse_meterpreter
+	  case reverse_meterpreter_answer
+			when '1'
+		  when '2'
+			when '99'
+			  case_main_menu
+			else
+        print_error("Bad Choice")
+				sleep(1)
+			  case_reverse_meterpreter_menu
+      end
+
+end
+def case_dump_lsass_menu
+  reverse_meterpreter_answer = reverse_meterpreter
+	  case reverse_meterpreter_answer
+			when '1'
+		  when '2'
+			when '99'
+			  case_main_menu
+			else
+        print_error("Bad Choice")
+				sleep(1)
+			  case_reverse_meterpreter_menu
+      end
+
+end
+begin
+  case_main_menu
+end
