f405a5a2a5c659dd61c703e666ea10af5b8017ab
b00stfr3ak <goduke44@gmail.com>
2013-09-03 13:26:48 -0700

    Skeleton Banner File
    Created a file to make banners for menu

---
+++ lib/banners.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+module Banners
+  def print_main_banner
+  
+  end
+  def print_reverse_meterpreter_banner
+  	
+  end
+  def print_dump_hash_banner
+  
+  end
+  def print_dump_lsass_banner
+  
+  end
+end
