eda6d681e8776bf2b2c33826b21cac65206be8f6
b00stfr3ak <goduke44@gmail.com>
2013-09-15 21:17:45 -0700

    Added color to banners

--- lib/menu.rb
+++ lib/menu.rb
@@ -5,7 +5,7 @@ include Banners
 module Menu
   def main_menu
     system('clear')
-    puts main_banner
+    print_banner_color(main_banner)
     print "
       \n1) Reverse Meterpreter \
       \n2) Dump Domain and Local Hashes \
@@ -16,7 +16,7 @@ module Menu
   end
   def reverse_meterpreter
     system('clear')
-    puts reverse_meterpreter_banner
+    print_banner_color(reverse_meterpreter_banner)
     puts 'This payload will create a reverse meterpreter shell through powershell bypassing all AV'
     print "
     \n1) Admin with UAC \
@@ -27,7 +27,7 @@ module Menu
   end
   def dump_hashes
     system('clear')
-    puts dump_hash_banner
+    print_banner_color(dump_hash_banner)
     puts 'This payload will dump Domand cached and Local Hashes and then push them to a listening server'
     print "
     \n1) Admin with UAC \
@@ -37,7 +37,7 @@ module Menu
   end
   def dump_lsass
     system('clear')
-    puts dump_lsass_banner
+    print_banner_color(dump_lsass_banner)
     puts 'This payload will dump the lsass process memory through powershell and then upload it to a listening server'
     print "
     \n1) Admin with UAC \
@@ -47,12 +47,13 @@ module Menu
   end
   def powershell_wget
     system('clear')
-    puts powershell_wget_banner
+    print_banner_color(powershell_wget_banner)
     puts 'This payload will download and executable from a webserver and execute it on the system'
     print "
     \n1) Admin with UAC\
     \n2) Admin with out UAC\
-    \n3) Low Priv\n"
+    \n3) Low Priv  \
+    \n99) Main Menu\n"
     Readline.readline('> ', true)
   end
 end
