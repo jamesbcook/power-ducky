b11b47e4cbc60454cde0a42cd5b423ec4db99164
b00stfr3ak <goduke44@gmail.com>
2015-03-19 13:14:35 -0700

    Added New CMD Option
    New CMD option to execute commands under CMD
    Added Menu options to show what IP and Port are in use

--- lib/guide.rb
+++ lib/guide.rb
@@ -7,14 +7,25 @@ class Guide < Menu
     # TODO: menu optoins need to include:
     # msf path, text path
     Menu.opts = {}
-
+    Menu.opts[:banner] = {
+      host: Menu.opts[:server] || 'No Server started',
+      ports: Menu.opts[:port] || 'No Ports used'
+    }
     launch!
   end
 
   def launch!
     options = []
-    options << 'PowerShell'
-    pick('1')
+    options << 'Main Menu'
+    options << '1) PowerShell'
+    options << '2) CMD'
+    main_menu(options, 'Exit')
+    print 'Choice'
+    begin
+      pick(rgets(': ').strip)
+    rescue SignalException
+      exit
+    end
   end
 
   def pick(choice)
@@ -23,6 +34,9 @@ class Guide < Menu
       @powershell ||= Powershell.new
       @powershell.launch!
     when '2'
+      @cmd ||= CMD.new
+      @cmd.launch!
+    when '3'
       exit
     end
     self.launch!
