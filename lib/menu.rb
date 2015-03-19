1f75caf1d15fe6ff693d95fd8dd96b6fc7608f5e
b00stfr3ak <goduke44@gmail.com>
2015-03-19 13:16:19 -0700

    Banner Added
    Added to main menu method to display the banner and all modules

--- lib/menu.rb
+++ lib/menu.rb
@@ -13,6 +13,39 @@ class Menu
     load_modules(files).to_a
   end
 
+  def main_menu(options, back = 'Main Menu')
+    banner
+    options << "#{options.length}) #{back}"
+    banner = []
+    Menu.opts[:banner].each { |k, v| banner << "#{k}: #{v}" }
+
+    # Determine if menu options or notes is longer
+    iterations = options.length > banner.length ? options.length : banner.length
+
+    # Print the menu while maintaining format regardless of whether either
+    # column is longer than the other
+    i = 0
+    while i < iterations
+      first, second = options[i], banner[i]
+      line = ''
+      # Blank string for nil to maintain formatting
+      first ||= ''
+      line << first.ljust(42)
+      line << second.rjust(40) if second
+      puts line
+      i += 1
+    end
+
+    puts
+  end
+
+  def banner
+    system('clear')
+    puts '*' * 81
+    puts '*' + ' ' * 34 + 'Power Ducky' + ' ' * 34 + '*'
+    puts '*' * 81
+  end
+
   def load_modules(class_files)
     before = ObjectSpace.each_object(Class).to_a
     class_files.each { |file| require file }
@@ -23,23 +56,20 @@ class Menu
     end
   end
 
-  def list_options(options)
-    system('clear')
-    options.each { |item| puts "#{item}" }
-    puts
-    choice = rgets('Choice: ')
-    select_option(choice)
-  end
-
   def select_option(selected_option)
+    return if selected_option.to_i > @modules.length
     @modules[selected_option.to_i - 1].new
   end
 
   def launch!
     options = []
     options << self.class.title
-    i = 1
-    @modules.each { |mod| options << "#{i}) #{mod.title}" && i += 1 }
-    list_options(options)
+    @modules.each_with_index do |mod, index|
+      options << "#{index + 1}) #{mod.title}"
+    end
+    main_menu(options)
+    select_option(rgets('Choice: '))
+    rescue SignalException
+      return
   end
 end
